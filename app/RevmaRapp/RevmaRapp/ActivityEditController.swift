9cc15e34fe5594351a2490d459b3aa16ee3241d5
Trenton Schulz <trenton.schulz@nr.no>
2014-11-25 17:23:57 +0100

    Start getting the activity name stuff going.
    Ugh! Still way to long to go, but we are getting closer.

--- app/RevmaRapp/RevmaRapp/ActivityEditController.swift
+++ app/RevmaRapp/RevmaRapp/ActivityEditController.swift
@@ -14,8 +14,10 @@ protocol ActivityEditControllerDelegate {
     func activityEditControllerDidSave(controller: ActivityEditController)
 }
 
-class ActivityEditController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ActivityEditController: UIViewController, UITableViewDataSource, UITableViewDelegate, ActivityNameTableControllerDelegate {
     
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+
     @IBOutlet weak var startDateButton: UIButton!
     
     @IBOutlet weak var durationButton: UIButton!
@@ -30,12 +32,13 @@ class ActivityEditController: UIViewController, UITableViewDataSource, UITableVi
     
     @IBOutlet weak var painSlider: UISlider!
     var managedObjectContext: NSManagedObjectContext?
-    var activityNames:[ActivityName] = []
-    var delegate:ActivityEditControllerDelegate? = nil
+    var activityNames: [ActivityName] = []
+    var delegate: ActivityEditControllerDelegate?
+    var activityName: ActivityName?
     
-    var activityItem: ActivityItem? {
+    var activityItem: ActivityItem! {
         didSet {
-            if (managedObjectContext == nil) {
+            if (managedObjectContext == nil && activityItem != nil) {
                 managedObjectContext = activityItem!.managedObjectContext
             }
             self.configureView()
@@ -46,7 +49,24 @@ class ActivityEditController: UIViewController, UITableViewDataSource, UITableVi
         if self.managedObjectContext == nil {
             return
         }
-        // Copy of all activity names? Sure.
+        
+        if activityItem.activity != nil {
+            activityName = activityItem.activity
+        }
+        
+        if activityNames.count == 0 {
+            fetchActivityNames()
+        }
+        
+        checkCanSave()
+    }
+    
+    func checkCanSave() {
+        doneButton.enabled = (activityName != nil)
+
+    }
+    
+    func fetchActivityNames() {
         let fetchRequest = NSFetchRequest(entityName: ActivityName.entityName())
         var error: NSError?
         if let results = self.managedObjectContext?.executeFetchRequest(fetchRequest, error: &error) {
@@ -54,14 +74,12 @@ class ActivityEditController: UIViewController, UITableViewDataSource, UITableVi
         } else {
             println("Unresolved error \(error?.localizedDescription), \(error?.userInfo)\n Attempting to get activity names")
         }
-
     }
     
     @IBAction func cancel(sender: UIBarButtonItem) {
         if delegate != nil {
             delegate!.activtyEditControllerDidCancel(self)
         }
-
     }
     
     @IBAction func done(sender: UIBarButtonItem) {
@@ -82,28 +100,44 @@ class ActivityEditController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func save() {
-        // write the stuff in and sync the database!!!
+        activityItem.activity = activityName
+        activityItem.pain = painSlider.value
+        activityItem.duty = dutySlider.value
+        activityItem.energy = energySlider.value
+        activityItem.mastery = masterySlider.value
+        activityItem.importance = masterySlider.value
+        var error: NSError?
+        managedObjectContext?.save(&error)
+        if let realError = error {
+            println("Unresolved error \(error?.localizedDescription), \(error?.userInfo)\n Attempting to get activity names")
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // TODO: Get the detail view hooked up.
+        if let whichSegue = segue.identifier {
+            switch (whichSegue) {
+            case "showActivityNames":
+                if let nameTableController = segue.destinationViewController.topViewController as? ActivityNameTableController {
+                    nameTableController.selectedName = activityName
+                    nameTableController.managedObjectContext = managedObjectContext
+                    nameTableController.delegate = self
+                }
+            default:
+                break;
+            }
+        }
     }
 
     // MARK TableViewDataSource
-    func tableView(tableView: UITableView,
-        numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return 1
     }
     
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             let tableCell = tableView.dequeueReusableCellWithIdentifier("ActivityEditActivityName", forIndexPath: indexPath) as UITableViewCell
-            
-            if let localActivity = activityItem {
-                if let activityName = localActivity.activity {
-                    tableCell.textLabel.text = activityName.name!
-                } else {
-                    tableCell.textLabel.text = NSLocalizedString("Tap to choose activity", comment: "Empty Activity Name")
-                }
-            } else {
-                tableCell.textLabel.text = NSLocalizedString("No activity!!!", comment: "No activity, this should not happen")
-            }
+
+            tableCell.textLabel.text = (activityName != nil) ? activityName?.name! : NSLocalizedString("Tap to choose activity", comment: "Empty Activity Name")
             return tableCell
     }
     
@@ -112,5 +146,14 @@ class ActivityEditController: UIViewController, UITableViewDataSource, UITableVi
             // ## Start the activity name viewer
     }
 
+    // MARK ActivityNameTableController
+    func activtyEditControllerDidCancel(controller: ActivityNameTableController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func activityEditControllerDidSave(controller: ActivityNameTableController) {
+        activityName = controller.selectedName
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
---
+++ app/RevmaRapp/RevmaRapp/ActivityNameTableController.swift
@@ -0,0 +1,96 @@
+//
+//  ActivityNameTableController.swift
+//  RevmaRapp
+//
+//  Created by Trenton Schulz on 25/11/14.
+//  Copyright (c) 2014 Norsk Regnesentral. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+protocol ActivityNameTableControllerDelegate {
+    func activtyEditControllerDidCancel(controller: ActivityNameTableController)
+    func activityEditControllerDidSave(controller: ActivityNameTableController)
+}
+
+class ActivityNameTableController : UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    
+    var managedObjectContext : NSManagedObjectContext?;
+        
+    var activityNames: [ActivityName] = []
+    var selectedName: ActivityName?
+    var delegate: ActivityNameTableControllerDelegate?
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if activityNames.count == 0 {
+            fetchActivityNames()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func fetchActivityNames() {
+        let fetchRequest = NSFetchRequest(entityName: ActivityName.entityName())
+        var error: NSError?
+        if let results = self.managedObjectContext?.executeFetchRequest(fetchRequest, error: &error) {
+            activityNames = results as [ActivityName]
+            activityNames.sort({ $0.name! < $1.name! })
+        } else {
+            println("Unresolved error \(error?.localizedDescription), \(error?.userInfo)\n Attempting to get activity names")
+        }
+    }
+
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        if delegate != nil {
+            delegate!.activtyEditControllerDidCancel(self)
+        }
+    }
+
+    // MARK: Table view controller functions
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activityNames.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ActivityNameListItem", forIndexPath: indexPath) as UITableViewCell
+        self.configureCell(cell, atIndexPath: indexPath)
+        return cell
+    }
+        
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        let name = activityNames[indexPath.row]
+        cell.textLabel.text = NSLocalizedString(name.name!, comment:"")
+        cell.accessoryType = (name == selectedName) ? UITableViewCellAccessoryType.Checkmark : UITableViewCellAccessoryType.None
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var oldRow = -1
+        var index = 0
+        for name in activityNames {
+            if name == selectedName {
+                oldRow = index;
+                break;
+            }
+            ++index
+        }
+        
+        if oldRow == indexPath.row {
+            return
+        }
+        
+        if let newCell = tableView.cellForRowAtIndexPath(indexPath) {
+            newCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            selectedName = activityNames[indexPath.row]
+        }
+
+        if let oldCell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow:oldRow, inSection: 0)) {
+            oldCell.accessoryType = UITableViewCellAccessoryType.None
+        }
+    }
+}
