b335964d766b330696f532f4bed3ef89e59800a7
Trenton Schulz <trenton.schulz@nr.no>
2015-11-17 15:06:03 +0100

    Put the first run of Kristin's text.
    Using pollen. Yeah! No slowing down here.

---
+++ app/RevmaRapp/RevmaRapp/help/helptext.html.pm
@@ -0,0 +1,58 @@
+#lang pollen
+
+◊(define rrap "RevmaRApp")
+◊h1{◊|rrap| Hjelptekst}
+
+◊h2{Hva er ◊|rrap|?}
+
+◊|rrap| er utviklet for deg som sliter med utmattelse. Den er et verktøy for å få bedre oversikt over hvordan daglige aktiviteter påvirker energi og mestring.  Ved hjelp av denne appen kan du registrere daglige aktiviteter og vurdere aktivitetene i forhold til energibruk, plikt, viktighet og mestring.
+
+Formålet med dette er å bidra til bedre oversikt over dine aktivitetsmønstre, energi, mestring og trivsel i hverdagen. Dette kan gi grunnlag for refleksjon rundt hvordan oppnå best mulig balanse mellom ulike aktiviteter i forhold til egne forutsetninger og livssituasjon.
+
+◊h2{Perioder}
+
+Det anbefales at du gjør en detaljert registrering av alle aktiviteter i minst 3 dager og inntil en uke. Velg gjerne en periode som inneholder en dag i en helg. Du kan gi registreringsperioden et navn som gir mening for deg.  
+
+Du kan gjenta den detaljerte registreringen på et senere tidspunkt, for eksempel om 2–3 måneder, for å se om det har skjedd en endring i positiv retning. Da kan du bruke funksjonen for å sammenligne perioder.
+
+◊h2{Aktivitetsperioder}
+
+Her får du oversikt over alle registrerte perioder. Du kan begynne å registrere en ny periode ved å trykke på pluss i høyre hjørne. 
+
+◊h2{Registrere aktivitet}
+
+Når du skal registrere aktiviteter kan du velge fra den forhåndsutfylte listen med aktiviteter. Du kan gjerne endre navn eller legge til nye aktiviteter slik at det passer din hverdag.
+
+Det anbefales å dele aktiviteter helt ned i times eller halvtimes intervaller.  Poenget med dette er å få innsikt i hvordan du bruker kreftene og hvor viktige de ulike aktivitetene er, og i hvilken grad aktivitetene er lystbetonte. Ved å gjennomføre en så detaljert registrering vil du kunne bli mer oppmerksom på variasjoner gjennom dagen. 
+
+Det kan være slitsomt å registrere såpass detaljert, og derfor anbefales en relativt kort registreringsperiode på mellom 3 dager og inntil en uke.  
+
+◊h2{Vurdering av aktivitet}
+
+For hver aktivitet angis tidspunkt og varighet. Ved hjelp av glideskalaen angis i hvilken grad aktiviteten er viktig for deg, om du kun følte plikt eller om den var mest lystbetont, og hvor mye energi du brukte på aktiviteten.  
+
+Legg merke til aktivitetssymbolet som endrer farge og størrelse på grunnlag av tidsbruk og hvordan du vurderer aktiviteten. Jo mer tid du brukte på aktiviteten, jo større blir symbolet. Hvis du brukte mye energi på aktiviteten blir symbolet rødt, mens hvis du fikk energi blir symbolet grønt. 
+
+De grønne aktivitetene er aktiviteter som du angir at er viktige for deg og som du opplever som lystbetonte oppgaver. Jo mørkere grønnfargen er, jo mer viktig og lystbetonte er aktiviteten.
+
+De røde aktiviteter er aktiviteter som du angir at er uviktige for deg og som du opplever mest som pliktoppgaver. Jo mørkere rødfargen er, jo mindre viktig og mer pliktbetont er aktiviteten.  I aktivitetsoversikten vises alle aktivitetene ved hjelp av aktivitetssymbolet. 
+
+Mestring…
+
+◊h2{Aktivitetsoversikt}
+
+Ved hjelp av den grafiske aktivitetsoversikten kan du få oversikt over alle aktiviteter som er registrert i en eller flere perioder. 
+
+◊h3{Velge perioder}
+
+Velg den eller de periodene du ønsker å studere.  De valgte periodene blir markert med en hake til høyre. 
+
+◊h3{Grafisk overskit}
+
+Grafen viser alle aktiviteter i den eller de periodene du har valgt. Hver aktivitet vises som et aktivitetssymbol. De aktivitetene som vises øverst til høyre er de aktivitetene som er viktigst og mest lystbetont, mens aktiviteter nederst til venstre er de aktivitetene som er mist viktige og som først og fremst oppleves som plikt. 
+
+Ved å trykke på et av de fire hoved områdene i grafen, øverst eller nederst til høyre eller venstre, kan du få listet opp alle aktivitetene i dette området.
+
+◊h3{Liste over aktiviteter}
+
+Ved å kutte ned på noen røde aktiviteter, og heller gjøre grønne aktiviteter som gir energi, kan du komme inn i en bedre sirkel som på sikt kan gi deg mer overskudd.  Dette kan føre til bedre mulighet til å delta i aktiviteter som er viktige for deg. 
---
+++ app/RevmaRapp/RevmaRapp/help/styles.css.pp
@@ -0,0 +1,45 @@
+#lang pollen
+
+@media all {
+    html {
+        font: 1em/1.25 "Helvetica Neue", Helvetica, Arial, Geneva, sans-serif;
+    }
+}
+
+@media all and (max-width:32em) {
+    html {
+        font-size: .9em;
+    }
+}
+
+@media all and (max-width:30em) {
+    html {
+        font-size: .8em;
+    }
+}
+
+@media all and (max-width:27em) {
+    html {
+        font-size: .75em;
+    }
+}
+
+@media all and (max-width:25em) {
+    html {
+        font-size: .65em;
+    }
+}
+
+body {
+    text-align: center;
+    margin: 2em;
+    padding: 0px;
+}
+
+#container {
+    max-width: 36em;
+    min-width: 25em;
+    margin: 0px auto;  /* Right And Left Margin Widths To Auto  */
+    list-style-position: outside;
+    text-align: left;
+}
