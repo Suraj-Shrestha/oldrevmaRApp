e8e2c1c5f8f354bccfcf8597584032c993987daf
Trenton Schulz <trenton.schulz@nr.no>
2016-01-04 13:15:48 +0100

    Slightly better help text.

--- app/RevmaRapp/RevmaRapp/help/helptext.html.pm
+++ app/RevmaRapp/RevmaRapp/help/helptext.html.pm
@@ -3,3 +3,14 @@
 
 ◊h1{◊(hash-ref metas 'title)}
 
+Denne er hovedsiden til hjelptekst til ◊|rrap|.
+
+◊h2{Oversikt}
+◊ul{
+◊li{◊a[#:href "what-is-revmarapp.html"]{Hva er ◊|rrap|?}}
+◊li{◊a[#:href "periods.html"]{Perioder}}
+◊li{◊a[#:href "activity-periods.html"]{Aktivitetsperioder}}
+◊li{◊a[#:href "register-activity.html"]{Registrer aktivitet}}
+◊li{◊a[#:href "evaluate-activity.html"]{Vurdering av aktivitet}}
+◊li{◊a[#:href "history.html"]{Aktivitetsoversikt}}
+}
